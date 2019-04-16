c08f18e089870da6c129e2a7170dae27bc524be6
Greg Stoll <greg@gregstoll.com>
2019-04-15 20:48:01 -0500

    showing tabs

--- cluesolver/src/App.tsx
+++ cluesolver/src/App.tsx
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            <Tabs>
+                <TabList>
+                    <Tab>Game setup</Tab>
+                    <Tab>Game info</Tab>
+                    <Tab>Undo and history</Tab>
+                    <Tab>Simulation</Tab>
+                </TabList>
+                <TabPanel>
+                    <div>Setup</div>
+                </TabPanel>
+                <TabPanel>
+                    <div>Info</div>
+                </TabPanel>
+                <TabPanel>
+                    <div>History</div>
+                </TabPanel>
+                <TabPanel>
+                    <div>Simulation</div>
+                </TabPanel>
+            </Tabs>
+        </div>
+      //<div className="App">
+      //  <header className="App-header">
+      //    <p>
+      //      Edit <code>src/App.tsx</code> and save to reload.
+      //    </p>
+      //    <a
+      //      className="App-link"
+      //      href="https://reactjs.org"
+      //      target="_blank"
+      //      rel="noopener noreferrer"
+      //    >
+      //      Learn React
+      //    </a>
+      //  </header>
+      //</div>
     );
   }
 }
