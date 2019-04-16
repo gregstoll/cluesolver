d671e49f28a255f361c177a6714b9f2b0f6e7117
Greg Stoll <greg@gregstoll.com>
2019-04-15 21:33:54 -0500

    adding a bunch of game setup stuff

--- cluesolver/src/App.tsx
+++ cluesolver/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, ChangeEvent } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import "react-tabs/style/react-tabs.css";
 import './App.css';
@@ -48,6 +48,161 @@ enum CardState {
     OwnedByCasefile = 2
 }
 
+interface NumberOfPlayerOptionProps {
+    thisNumberOfPlayers: number,
+    numberOfPlayers: number,
+    allowChange: boolean,
+    setNumberOfPlayers: (numberOfPlayers: number) => void
+}
+
+class NumberOfPlayerOption extends Component<NumberOfPlayerOptionProps, {}> {
+    handleChange() {
+        this.props.setNumberOfPlayers(this.props.thisNumberOfPlayers);
+    }
+    render() {
+        var id = "numberOfPlayersInput" + this.props.thisNumberOfPlayers;
+        return <span><input type="radio" name="numberOfPlayer" id={id} value="{this.props.thisNumberOfPlayers}" checked={this.props.thisNumberOfPlayers == this.props.numberOfPlayers} onChange={this.handleChange} disabled={!this.props.allowChange}/><label htmlFor={id}> {this.props.thisNumberOfPlayers}</label></span>;
+    }
+}
+
+interface PlayerInfo {
+    name: string,
+    numberOfCards: number
+}
+
+interface NumberOfPlayersProps {
+    playerInfos: Array<PlayerInfo>,
+    allowChange: boolean,
+    setNumberOfPlayers: (numberOfPlayers: number) => void
+}
+
+class NumberOfPlayers extends Component<NumberOfPlayersProps, {}> {
+    render() {
+        var options = [];
+        for (var i = MIN_PLAYERS; i <= MAX_PLAYERS; ++i) {
+            options.push(<NumberOfPlayerOption thisNumberOfPlayers={i} numberOfPlayers={this.props.playerInfos.length} key={i} setNumberOfPlayers={this.props.setNumberOfPlayers} allowChange={this.props.allowChange}/>);
+        }
+        return <div><span>Number of players:</span><form style={{display: 'inline'}}>{options}</form></div>;
+    }
+}
+
+interface PlayerNumberOfCardsProps {
+    allowChange: boolean,
+    num: number,
+    setNumberOfCards: (numberOfCards: number) => void
+}
+
+class PlayerNumberOfCards extends Component<PlayerNumberOfCardsProps, {}> {
+    handleChange(e: ChangeEvent<HTMLSelectElement>) {
+        this.props.setNumberOfCards(parseInt(e.target.value, 10));
+    }
+    render() {
+        var options = [];
+        for (var i = MIN_CARDS; i <= MAX_CARDS; i++) {
+            options.push(<option value={i} key={i}>{i}</option>);
+        }
+        return <select onChange={this.handleChange} disabled={!this.props.allowChange} value={this.props.num}>{options}</select>;
+    }
+}
+
+interface PlayerInfoComponentProps {
+    index: number,
+    info: PlayerInfo,
+    allowChange: boolean,
+    setNumberOfCards: (index: number, numberOfCards: number) => void,
+    setPlayerName: (index: number, name: string) => void,
+}
+
+class PlayerInfoComponent extends Component<PlayerInfoComponentProps, {}> {
+    setNumberOfCards(numberOfCards: number) {
+        this.props.setNumberOfCards(this.props.index, numberOfCards);
+    }
+    setPlayerName(e: ChangeEvent<HTMLInputElement>) {
+        this.props.setPlayerName(this.props.index, e.target.value);
+    }
+    render() {
+        return <div>Name:<input type="text" value={this.props.info.name} onChange={this.setPlayerName} /> Number of cards:<PlayerNumberOfCards num={this.props.info.numberOfCards} setNumberOfCards={this.setNumberOfCards} allowChange={this.props.allowChange} /></div>;
+    }
+}
+
+interface PlayerListProps {
+    playerInfo: Array<PlayerInfo>,
+    allowChange: boolean,
+    setNumberOfCards: (index: number, numberOfCards: number) => void,
+    setPlayerName: (index: number, name: string) => void,
+}
+
+class PlayerList extends Component<PlayerListProps, {}> {
+    render() {
+        let infos = [];
+        for (let i = 0; i < this.props.playerInfo.length; ++i) {
+            infos.push(<PlayerInfoComponent index={i} key={i} info={this.props.playerInfo[i]} setNumberOfCards={this.props.setNumberOfCards} setPlayerName={this.props.setPlayerName} allowChange={this.props.allowChange}/>);
+        }
+        return <div>{infos}</div>;
+    }
+}
+
+interface NumberOfCardsValidatorProps {
+    playerInfo: Array<PlayerInfo>
+}
+
+class NumberOfCardsValidator extends Component<NumberOfCardsValidatorProps, {}> {
+    render() {
+        // TODO - do this better?
+        var totalNumberOfCards = this.props.playerInfo.reduce(function (previousValue, currentValue) {
+            return previousValue + currentValue.numberOfCards;
+        }, 0);
+        var badNumberOfCardsElem = null;
+        if (totalNumberOfCards != TOTAL_CARDS_FOR_PLAYERS) {
+            badNumberOfCardsElem = <span className="warning">Total number of cards must total {TOTAL_CARDS_FOR_PLAYERS}! (not {totalNumberOfCards})</span>;
+        }
+        return <div>{badNumberOfCardsElem}</div>;
+    }
+}
+
+interface GameSetupProps {
+    newSession: () => void,
+    setGameString: (gameString: string) => void,
+    playerInfos: Array<PlayerInfo>,
+    haveEnteredData: boolean,
+    session: string,
+    setNumberOfCards: (index: number, numberOfCards: number) => void,
+    setPlayerName: (index: number, name: string) => void,
+    setNumberOfPlayers: (numberOfPlayers: number) => void
+}
+
+interface GameSetupState {
+    loadGameString: string
+}
+
+class GameSetup extends Component<GameSetupProps, GameSetupState> {
+    constructor(props: GameSetupProps) {
+        super(props);
+        this.state = {
+            loadGameString: ""
+        }
+    }
+    handleRestartGame() {
+        this.props.newSession();
+    }
+    handleLoadGameStringChange(e: ChangeEvent<HTMLInputElement>) {
+        this.setState({loadGameString: e.target.value});
+    }
+    handleSetLoadGameString() {
+        this.props.setGameString(this.state.loadGameString);
+    }
+    render() {
+        return <div>
+            <NumberOfPlayers playerInfos={this.props.playerInfos} setNumberOfPlayers={this.props.setNumberOfPlayers} allowChange={!this.props.haveEnteredData}/>
+            <PlayerList playerInfo={this.props.playerInfos} setNumberOfCards={this.props.setNumberOfCards} setPlayerName={this.props.setPlayerName} allowChange={!this.props.haveEnteredData}/>
+            <NumberOfCardsValidator playerInfo={this.props.playerInfos} />
+            <button type="button" onClick={this.handleRestartGame} disabled={!this.props.haveEnteredData}>Restart game</button>
+            <div>Current game state (for loading later): {this.props.session}</div>
+            <div>Load game state: <input type="text" onChange={this.handleLoadGameStringChange} value={this.state.loadGameString}/></div>
+            <button type="button" onClick={this.handleSetLoadGameString}>Load game</button>
+          </div>;
+    }
+}
 
 class App extends Component {
   render() {
