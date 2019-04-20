69100884dbd8a6b4d0440a6d7e97f6b67f1a7cce
Greg Stoll <greg@gregstoll.com>
2019-04-19 20:06:50 -0500

    remove comments

--- cluesolver/src/App.tsx
+++ cluesolver/src/App.tsx
@@ -501,21 +501,6 @@ class App extends Component<{}, AppState> {
                     </TabPanel>
                 </Tabs>
             </div>
-          //<div className="App">
-          //  <header className="App-header">
-          //    <p>
-          //      Edit <code>src/App.tsx</code> and save to reload.
-          //    </p>
-          //    <a
-          //      className="App-link"
-          //      href="https://reactjs.org"
-          //      target="_blank"
-          //      rel="noopener noreferrer"
-          //    >
-          //      Learn React
-          //    </a>
-          //  </header>
-          //</div>
         );
     }
 }
