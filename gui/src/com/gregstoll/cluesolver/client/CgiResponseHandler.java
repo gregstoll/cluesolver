86b8ae0bb2eff2b31fb5239ca5b962a88cad9b32
Greg Stoll <greg@gregstoll.com>
2019-04-25 21:19:35 -0500

    remove stale Google Web Toolkit (GWT) code

--- gui/src/com/gregstoll/cluesolver/client/CgiResponseHandler.java
+++
@@ -1,6 +0,0 @@
-package com.gregstoll.cluesolver.client;
-
-public interface CgiResponseHandler {
-    public void onSuccess(String body);
-    public void onError(Throwable ex);
-}
--- gui/src/com/gregstoll/cluesolver/client/ConfirmHandler.java
+++
@@ -1,5 +0,0 @@
-package com.gregstoll.cluesolver.client;
-
-public interface ConfirmHandler {
-    public void doAction(ClueSolver solver);
-}
