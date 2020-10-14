cae699debdd25f3e51110fb81b2bd5dc4b7b4775
gregstoll <greg@gregstoll.com>
2020-10-13 22:18:33 -0500

    better benchmark

--- clueengine_rust/src/bin/benchmark.rs
+++ clueengine_rust/src/bin/benchmark.rs
@@ -3,9 +3,10 @@ use clueengine::ClueEngine;
 
 fn main() {
     let start = Instant::now();
-    let clue_engine = ClueEngine::load_from_string("45CPQRS-ABDEFGHIJKLMNOTU.5AGIMT-BCDEFHJKLNOPQRSU.4FK-ACDGIJLMPQRST-EO.4DL-ABCFGIJKMPQRST.3J-ACDFGHIKLMPQRST.").unwrap();
+    //let clue_engine = ClueEngine::load_from_string("45CPQRS-ABDEFGHIJKLMNOTU.5AGIMT-BCDEFHJKLNOPQRSU.4FK-ACDGIJLMPQRST-EO.4DL-ABCFGIJKMPQRST.3J-ACDFGHIKLMPQRST.").unwrap();
+    let clue_engine = ClueEngine::load_from_string("63A-.3-A.3-A.3-A.3-A.3-A.3-A.").unwrap();
 
-    for _ in 0..50 {
+    for _ in 0..10 {
         let simulation_data = clue_engine.do_simulation();
         let (card, data) = simulation_data.iter().next().unwrap();
         // Print something to make sure nothing gets optimized out
