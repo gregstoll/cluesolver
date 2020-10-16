06ee1628e8bc7cc8b044e5d93f1d2b3828b1b452
gregstoll <greg@gregstoll.com>
2020-10-15 21:32:42 -0500

    increase python benchmark

--- benchmark.py
+++ benchmark.py
@@ -4,7 +4,7 @@ import clueengine
 start = time.time()
 (ce, s) = clueengine.ClueEngine.loadFromString("63A-.3-A.3-A.3-A.3-A.3-A.3-A.")
 
-for i in range(10):
+for i in range(100):
     simData = ce.getSimulationData()
     print(simData['Conservatory'])
 end = time.time()
